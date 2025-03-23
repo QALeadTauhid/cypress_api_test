0d792105df5b1d0524593937c055aa787015ce4f
Tauhidul <testaccoffice3@gmail.com>
2025-03-23 12:59:12 +0600

    updated code

--- cypress/e2e/API Testing/QueryParam.cy.js
+++ cypress/e2e/API Testing/QueryParam.cy.js
@@ -1,10 +1,24 @@
+// const { expect } = require("chai");
+
 describe('API Testing', ()=>{
+    const queryparam = {page:2}
     it('Passing query param', ()=>{
 
-        cypress.request({
+        cy.request({
             method: "GET",
             url: 'https://reqres.in/api/users',
-            qs: {page=2}
+            // qs: {page:2},
+            qs: queryparam
+        })
+        .then((response)=>{
+            expect(response.status).to.eq(200);
+            expect(response.status).equal(200);
+            expect(response.body.page).to.eq(2);
+            expect(response.body.data).has.length(6);
+            expect(response.body.data[0]).have.property('id',7);
+            expect(response.body.data[0]).has.property('first_name', "Michael")
+
+
         })
 
 
